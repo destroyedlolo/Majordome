73138732da516a5e30a0fd38f04a28c1984a3fe1
destroyedlolo <l_faillie@yahoo.com>
2016-06-26 02:37:18 +0200

    Add consignes

---
+++ Consignes.lua
@@ -0,0 +1,30 @@
+-- Consignes
+
+--
+-- Topics
+--
+
+HLEVE='Majordome/HLever'
+HCOUCHE='Majordome/HCoucher'
+
+--
+-- Valeurs par défaut
+--
+
+SelShared.set( HLEVE, 8.00 )	-- Ne doit pas être < a 1:00
+SelShared.set( HCOUCHE, 21.10 )	-- pour les enfants
+
+--
+-- Actions
+--
+
+function chgConsignes()
+	SelLog.log("Heure levé :" .. SelShared.get( HLEVE ) )
+	SelLog.log("Heure couché :" .. SelShared.get( HCOUCHE ) )
+
+	SubTasks( Tasks['Consignes'] )
+end
+
+Tasks['Consignes']={}
+table.insert( Topics, { topic = HLEVE, trigger=chgConsignes, trigger_once=true } )
+table.insert( Topics, { topic = HCOUCHE, trigger=chgConsignes, trigger_once=true } )
