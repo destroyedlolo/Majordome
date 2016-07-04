5039bbfbeb7a2b347f4c5a86657a50e93b8a86e1
destroyedlolo <l_faillie@yahoo.com>
2016-07-05 00:05:22 +0200

    Handle kids shutters

--- Chambre1er.lua
+++ Chambre1er.lua
@@ -4,48 +4,59 @@
 -- Topics
 --
 
-local TChOceane='maison/Volet/chOceane'
-local TChJoris='maison/Volet/chJoris'
-local TChParents='maison/Volet/chParents'
+local TpChOceane='maison/Volet/chOceane'
+local TpChJoris='maison/Volet/chJoris'
+local TpChParents='maison/Volet/chParents'
+
+-- Plage durant laquelle les volets peuvent se baisser
+-- automatiquement pour conserver de la fraîcheur
+-- Format hh.mm
+local HDebFraicheurAutoV=11	-- Auto pour les vacances
+local HDebFraicheurOJAutoT=9.3
+local HDebFraicheurParentT=8.4
+local HFinFraicheur1erAuto=17.3
+
+-- Température à partir de laquelle les volets se fermeront
+local LimiteTemperature=21
 
 --
 -- Actions
 --
 
 function OuvreChOceane()
-	VoletOuvre(TChOceane, 'Chambre Océane')
+	VoletOuvre(TpChOceane, 'Chambre Océane')
 end
 
 function FermeChOceane()
-	VoletFerme(TChOceane, 'Chambre Océane')
+	VoletFerme(TpChOceane, 'Chambre Océane')
 end
 
 function MyChOceane()
-	VoletMy(TChOceane, 'Chambre Océane')
+	VoletMy(TpChOceane, 'Chambre Océane')
 end
 
 function OuvreChJoris()
-	VoletOuvre(TChJoris, 'Chambre Joris')
+	VoletOuvre(TpChJoris, 'Chambre Joris')
 end
 
 function FermeChJoris()
-	VoletFerme(TChJoris, 'Chambre Joris')
+	VoletFerme(TpChJoris, 'Chambre Joris')
 end
 
 function MyChJoris()
-	VoletMy(TChJoris, 'Chambre Joris')
+	VoletMy(TpChJoris, 'Chambre Joris')
 end
 
 function OuvreChParents()
-	VoletOuvre(TChParents, 'Chambre Parents')
+	VoletOuvre(TpChParents, 'Chambre Parents')
 end
 
 function FermeChParents()
-	VoletFerme(TChParents, 'Chambre Parents')
+	VoletFerme(TpChParents, 'Chambre Parents')
 end
 
 function MyChParents()
-	VoletMy(TChParents, 'Chambre Parents')
+	VoletMy(TpChParents, 'Chambre Parents')
 end
 
 --
@@ -84,6 +95,7 @@ function determinePlanning1er()
 			tmrRemoveEntry(tbl_timers, OuvreChParents)
 		end
 
+			-- Fermeture le soir
 		local t = SelShared.get(TSunSet)
 		h = DEC2DMS(DMS2DEC(SelShared.get( HCOUCHE )) - DMS2DEC(0.15))
 		if t > h then
@@ -102,6 +114,32 @@ function determinePlanning1er()
 			tmrAddEntry( tbl_timers, t, FermeChParents )
 			SelLog.log("Fermeture des chambres du haut à " .. h)
 		end
+
+			-- Conservation de la fraîcheur
+		local dt = os.date("*t")
+		local cur = dt.hour + dt.min/100
+
+		local hdebut = (SelShared.get(MODE) == 'Travail') and HDebFraicheurOJAutoT or HDebFraicheurAutoV
+		if cur < hdebut then	-- avant
+			SelLog.log("La température des chambres des enfants sera surveillée à partir de ".. hdebut)
+			SelLog.log("La surveillance se terminera à ".. HFinFraicheur1erAuto)
+
+			tmrAddEntry( tbl_timers, hdebut, LanceFraicheurChOJAuto)
+			tmrAddEntry( tbl_timers, HFinFraicheur1erAuto, FinFraicheurChOJAuto)
+			ColRemoveFunc( Tasks['TChOceane'], FraicheurChOJAuto)
+		elseif cur < HFinFraicheur1erAuto then	-- pendant
+			LanceFraicheurChOJAuto()
+			SelLog.log("La surveillance se terminera à ".. HFinFraicheur1erAuto)
+			tmrAddEntry( tbl_timers, HFinFraicheur1erAuto, FinFraicheurChOJAuto)
+		else -- apres
+			FinFraicheurChOJAuto()
+		end
+
+		local hdebut = (SelShared.get(MODE) == 'Travail') and HDebFraicheurParentT or HDebFraicheurAutoV
+		if cur < hdebut then	-- avant
+		elseif cur < HFinFraicheur1erAuto then	-- pendant
+		else -- apres
+		end
 	end
 
 	SelShared.PushTask( rethingTimerCron, SelShared.TaskOnceConst("LAST"))
@@ -110,3 +148,32 @@ end
 table.insert( Tasks['Saison'], determinePlanning1er )
 table.insert( Tasks['Mode'], determinePlanning1er )
 table.insert( Tasks['Consignes'], determinePlanning1er )
+
+--
+-- Gestion des températures
+-- 
+
+function FraicheurChOJAuto()
+	if SelShared.get( TChOceane ) > LimiteTemperature then
+		SelLog.log("TChOceane : " .. SelShared.get( TChOceane ) .. ", les volets se ferment")
+		MyChOceane()
+		MyChJoris()
+		FinFraicheurChOJAuto()
+	end
+end
+
+function LanceFraicheurChOJAuto()
+	SelLog.log("Début de la surveillance de la température des chambres des enfants")
+	
+	tmrRemoveEntry(tbl_timers, LanceFraicheurChOJAuto)
+	ColAddFunc( Tasks['TChOceane'], FraicheurChOJAuto)
+end
+
+function FinFraicheurChOJAuto()
+	SelLog.log("Fin de la surveillance de la température des chambres des enfants")
+
+	tmrRemoveEntry(tbl_timers, LanceFraicheurChOJAuto)
+	tmrRemoveEntry(tbl_timers, FinFraicheurChOJAuto)
+	ColRemoveFunc( Tasks['TChOceane'], FraicheurChOJAuto)
+end
+
