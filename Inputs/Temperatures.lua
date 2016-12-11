4d94c6e0c453d381a069e4a46f2e119074cf36db
destroyedlolo <l_faillie@yahoo.com>
2016-12-11 16:29:32 +0100

    All temperatures added

--- Inputs/Temperatures.lua
+++ Inputs/Temperatures.lua
@@ -1,3 +1,9 @@
 -- Home temperatures to follow
 
 TExterieur = MQTTinputs('TExterieur', 'maison/Temperature/Dehors', Topic2Number)
+TSalon = MQTTinputs('TSalon', 'maison/Temperature/Salon', Topic2Number)
+TBureau = MQTTinputs('TBureau', 'maison/Temperature/Bureau', Topic2Number)
+
+TChOceane = MQTTinputs('TChOceane', 'maison/Temperature/Chambre Oceane', Topic2Number)
+TChJoris = MQTTinputs('TChJoris', 'maison/Temperature/Chambre Joris', Topic2Number)
+TChParents = MQTTinputs('TChParents', 'maison/Temperature/Chambre Parents', Topic2Number)
--- Inputs/Test.lua
+++ Inputs/Test.lua
@@ -5,7 +5,8 @@ local function CTest()	-- Test call to test inheritance
 	local function arrived() -- Tasks called when Test is updated
 		print('arrived', SelShared.get('Test'))
 	end
-	
+
+		-- It's only a test : it has nothing to do in input
 	local function tempchanged() -- When TExterieur is changed
 		print("TExt", TExterieur.get() or 'not set')
 	end
