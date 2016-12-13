ad0e6bd91a584fc89ae8afd55c5e1dd3b77e7e38
destroyedlolo <l_faillie@yahoo.com>
2016-12-13 01:04:25 +0100

    Cosmetic too

--- Supports/MQTTinputs.lua
+++ Supports/MQTTinputs.lua
@@ -27,7 +27,7 @@ function MQTTinputs(aname, atpc, afunc)
 
 	function self.TasksAdd( func )
 		if not func then
-			SelLog.log("*D**E* MQTTinputs.TasksAdd( NULL )")
+			SelLog.log("*E* MQTTinputs.TasksAdd( NULL )")
 			return
 		end
 
