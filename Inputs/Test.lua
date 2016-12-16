07b21e228295fe1c1df762399a2aa48a5158be7c
destroyedlolo <l_faillie@yahoo.com>
2016-12-17 00:22:38 +0100

    Add configurable debug informations

--- Inputs/Test.lua
+++ Inputs/Test.lua
@@ -1,44 +1,47 @@
-local function ITest()	-- Test call to test inheritance
-	local self = MQTTinputs('Test', 'Test', Topic2Number)
+if DEBUG then
 
-	-- methods
-	local function arrived() -- Tasks called when Test is updated
-		print('arrived', SelShared.get('Test'))
+	local function ITest()	-- Test call to test inheritance
+		local self = MQTTinputs('Test', 'Test', Topic2Number)
+
+		-- methods
+		local function arrived() -- Tasks called when Test is updated
+			print('arrived', SelShared.get('Test'))
+		end
+
+			-- It's only a test : it has nothing to do in input
+		local function tempchanged() -- When TExterieur is changed
+			print("TExt", TExterieur.get() or 'not set')
+		end
+
+		-- initialiser
+		self.TaskAdd( arrived )
+		TExterieur.TaskAdd( tempchanged )
 	end
 
-		-- It's only a test : it has nothing to do in input
-	local function tempchanged() -- When TExterieur is changed
-		print("TExt", TExterieur.get() or 'not set')
+	tst = ITest()
+
+
+		-- Timer test
+	function bip1()
+		print 'bip 1'
 	end
 
-	-- initialiser
-	self.TaskAdd( arrived )
-	TExterieur.TaskAdd( tempchanged )
-end
+	function bip2()
+		print 'bip 2'
+	end
 
-tst = ITest()
+	function bip3()
+		print 'bip 3'
+	end
 
+	local dt = os.date("*t")
+	local cur = dt.hour + dt.min/100
+	local t = DEC2DMS(DMS2DEC(cur) + DMS2DEC(0.01))	-- Dans 1'
 
-	-- Timer test
-function bip1()
-	print 'bip 1'
-end
-
-function bip2()
-	print 'bip 2'
-end
-
-function bip3()
-	print 'bip 3'
-end
-
-local dt = os.date("*t")
-local cur = dt.hour + dt.min/100
-local t = DEC2DMS(DMS2DEC(cur) + DMS2DEC(0.01))	-- Dans 1'
-
-Timer.TaskAdd( t, bip1 );
+	Timer.TaskAdd( t, bip1 );
 	
-t = DEC2DMS(DMS2DEC(cur) + DMS2DEC(0.02))	-- Dans 2'
+	t = DEC2DMS(DMS2DEC(cur) + DMS2DEC(0.02))	-- Dans 2'
 
-Timer.TaskAdd( t, bip2 );
-Timer.TaskAdd( t, bip3 );
+	Timer.TaskAdd( t, bip2 );
+	Timer.TaskAdd( t, bip3 );
+end
--- LocalConfiguration.lua
+++ LocalConfiguration.lua
@@ -1,7 +1,8 @@
 -- This file holds only configuration local to this instance
 
 -- Internal
-VERBOSE=true
+VERBOSE=true	-- if true, log is generated
+DEBUG=true		-- if true, include debug and test messages
 
 -- BROKERHOST='tcp://bPI.chez.moi:1883'
 BROKERHOST='tcp://torchwood.chez.moi:1883'
