43cdde35f0719dead39a9c9c2b91d6d5a3daacbc
destroyedlolo <l_faillie@yahoo.com>
2016-12-18 02:11:13 +0100

    Season working

---
+++ Inputs/Saison.lua
@@ -0,0 +1,28 @@
+-- Seasons
+
+Saison = MQTTinputsDefLog('Saison', 'Majordome/Saison', nil, 'Intersaison')
+SaisonHier = MQTTinputsDefLog('Saison Hier', 'Majordome/Saison/Hier', nil, 'Intersaison')
+
+--
+-- Actions
+--
+
+local function DetermineSaison()
+	SelLog.log("Détermination de la saison")
+
+	SelLog.log("T°Ext = " .. TExterieur.get() .. " TSalon = " .. TSalon.get())
+
+	if TExterieur.get() < 2 then
+		Brk:Publish( Saison.getTopic(), 'Hiver', true )
+	elseif TSalon.get() > 21.5 and TExterieur.get() > 15 then
+		Brk:Publish( Saison.getTopic(), 'Ete', true )
+	else
+		Brk:Publish( Saison.getTopic(), 'Ete', true )
+	end
+end
+Timer.TaskAdd( 22.00, DetermineSaison )	-- Season is calculated at 22:00
+
+local function UpdYesterdaySeason()
+	Brk:Publish( SaisonHier.getTopic(), Saison.get(), true )
+end
+Saison.TaskAdd(UpdYesterdaySeason)
