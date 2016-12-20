46ce2a6b0c989dfd51251dcf83d67598b4b0f5b2
destroyedlolo <l_faillie@yahoo.com>
2016-12-20 16:29:01 +0100

    Cat flap handling

---
+++ Actions/20_Chat.lua
@@ -0,0 +1,27 @@
+-- Cat flap
+
+function AChat()
+	local self = SShutter('Trappe du chat', 'maison/Volet/Cuisine/Chat')
+
+	-- methods
+	local function determinePlaning()
+		if Saison.get() == 'Hiver' then
+			SelLog.log("'My' du chat à 21h")
+			Timer.TaskAdd( 21.0, self.My )
+		elseif ModeDemain.get() == 'Travail' then	-- Eté ou Intersaison
+			SelLog.log("'My' du chat à 1h")
+			Timer.TaskAdd( 1.0, self.My )
+		elseif ModeDemain.get() == 'Vacances' then
+			SelLog.log("'My' du chat à 2h")
+			Timer.TaskAdd( 2.0, self.My )
+		else
+			Timer.TaskRemove( self.My )
+		end
+	end
+	Saison.TaskAdd( determinePlaning )
+	Mode.TaskAdd( determinePlaning )
+	
+	return self
+end
+Chat = AChat()
+
