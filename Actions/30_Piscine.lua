7da08a16092e52835d226f2b4fe09ada1b77e499
destroyedlolo <l_faillie@yahoo.com>
2017-09-18 00:18:01 +0200

    Add a status topic to ModePiscine

--- Actions/30_Piscine.lua
+++ Actions/30_Piscine.lua
@@ -11,17 +11,21 @@ function APiscine(
 		elseif SelShared.get( ModePiscine.getTopic() ) == 'Forcé' then
 			pubLog(self.getName() .. " : Forcé", 'Action')
 			Brk:Publish(atpc, 1)
+			Brk:Publish(ModePiscine.getTopic() .. '/status', 'Forcé', true)
 		elseif SelShared.get( ModePiscine.getTopic() ) == 'Arret' then
 			pubLog(self.getName() .. " : Arrêté", 'Action')
 			Brk:Publish(atpc, 0)
+			Brk:Publish(ModePiscine.getTopic() .. '/status', 'Arret', true)
 		else -- 'Heure Creuse'
 			if not SelShared.get( TarifElectricite.getTopic() ) then
 				pubLog(self.getName() .. " : Tarif électricité non renseigné", 'Erreur')
 			elseif SelShared.get( TarifElectricite.getTopic() ) == 'HC..' then
 				pubLog(self.getName() .. " : Pompe activée", 'Action')
 				Brk:Publish(atpc, 1)
+				Brk:Publish(ModePiscine.getTopic() .. '/status', 'HC1', true)
 			else
 				pubLog(self.getName() .. " : Pompe stoppée", 'Action')
+				Brk:Publish(ModePiscine.getTopic() .. '/status', 'HC0', true)
 				Brk:Publish(atpc, 0)
 			end
 		end
