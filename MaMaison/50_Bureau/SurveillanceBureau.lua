d1ddabcba55a0cfb26520861a06adf398f7c1a21
destroyedlolo <l_faillie@yahoo.com>
2018-10-25 00:03:13 +0200

    Supprime une erreur de conversion

--- MaMaison/50_Bureau/SurveillanceBureau.lua
+++ MaMaison/50_Bureau/SurveillanceBureau.lua
@@ -7,7 +7,7 @@ local TLIMIT=21.5
 local TemperatureBureau = MajordomeMQTTTopic.find("TemperatureBureau", true)
 local VoletBureau = MajordomeMQTTTopic.find("CmdVoletBureau", true)
 
-if MAJORDOME_PAYLOAD >= TLIMIT then
+if tonumber(MAJORDOME_PAYLOAD) >= TLIMIT then
 	SelLog.log('I', "Température du bureau, "..TLIMIT.." > à la consigne, Fin de la surveillance")
 	SelLog.log('A', 'Fermeture "My" du volet du bureau')
 	VoletBureau:Publish("My")
