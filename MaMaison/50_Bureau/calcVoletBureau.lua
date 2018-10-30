4b2e051a1f0514b78101733c9dee7858595a19b8
destroyedlolo <l_faillie@yahoo.com>
2018-10-30 02:03:25 +0100

    Prend en compte le mode manuel

--- MaMaison/50_Bureau/calcVoletBureau.lua
+++ MaMaison/50_Bureau/calcVoletBureau.lua
@@ -1,4 +1,5 @@
 -- Détermine les heures d'ouverture et de fermeture du bureau
+-->> listen=Mode
 -->> listen=Saison
 -->> listen=LeverSoleil
 -->> listen=CoucherSoleil
@@ -23,18 +24,28 @@ local FermetureVoletBureau = MajordomeTimer.find("FermetureVoletBureau", true)
 local hl,ml = string.match(string.gsub( SelShared.Get('LeverSoleil'), '%.', ':'), "(%d+):(%d+)")
 local hc,mc = string.match(string.gsub( SelShared.Get('CoucherSoleil'), '%.', ':'), "(%d+):(%d+)")
 
-if SelShared.Get("Saison") == 'Hiver' then
-	OuvertureVoletBureau:setAtHM( hl+1, ml )
-	hl,ml = OuvertureVoletBureau:getAtHM()
-	SelLog.log('I', "Le volet du bureau s'ouvrira à ".. hl ..":".. ml)
-	
-	FermetureVoletBureau:setAtHM( hc-1, mc )
-	hc,mc = FermetureVoletBureau:getAtHM()
-	SelLog.log('I', "Le volet du bureau se fermera à ".. hc ..":".. mc)
-else -- Autre saison
-	OuvertureVoletBureau:setAtHM( hl, ml )
-	SelLog.log('I', "Le volet du bureau s'ouvrira avec le soleil à ".. hl ..":".. ml)
+if SelShared.Get("Mode") == 'Manuel' then
+	OuvertureVoletBureau:Disable()
+	FermetureVoletBureau:Disable()
 
-	FermetureVoletBureau:setAtHM( hc, mc )
-	SelLog.log('I', "Le volet du bureau se fermera avec le soleil à ".. hc ..":".. mc)
+	SelLog.log('I', "Le volet du bureau n'est soumis à aucun automatisme")
+else
+	OuvertureVoletBureau:Enable()
+	FermetureVoletBureau:Enable()
+
+	if SelShared.Get("Saison") == 'Hiver' then
+		OuvertureVoletBureau:setAtHM( hl+1, ml )
+		hl,ml = OuvertureVoletBureau:getAtHM()
+		SelLog.log('I', "Le volet du bureau s'ouvrira à ".. hl ..":".. ml)
+	
+		FermetureVoletBureau:setAtHM( hc-1, mc )
+		hc,mc = FermetureVoletBureau:getAtHM()
+		SelLog.log('I', "Le volet du bureau se fermera à ".. hc ..":".. mc)
+	else -- Autre saison
+		OuvertureVoletBureau:setAtHM( hl, ml )
+		SelLog.log('I', "Le volet du bureau s'ouvrira avec le soleil à ".. hl ..":".. ml)
+
+		FermetureVoletBureau:setAtHM( hc, mc )
+		SelLog.log('I', "Le volet du bureau se fermera avec le soleil à ".. hc ..":".. mc)
+	end
 end
