7bcef0c03f77e0d2cec19ba64892353a488be085
destroyedlolo <l_faillie@yahoo.com>
2019-03-16 17:47:09 +0100

    Log to file

--- MaMaison/00_Majordome/LogToFile.lua
+++ MaMaison/00_Majordome/LogToFile.lua
@@ -1,6 +1,6 @@
 -- Initialise log to file will every day log rotation
 -->> when=MidnightOrAtLaunch
-local _,err = SelLog.init('/tmp/'.. MAJORDOME_ClientID ..'.log_' .. os.date('%Y%m%d'), false)
+local _,err = SelLog.init('/tmp/'.. MAJORDOME_ClientID ..'.log_' .. os.date('%Y%m%d'), true)
 
 if err then
 	SelLog.log("*E* Log file creation : ".. err)
