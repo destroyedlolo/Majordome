578d57bd3ea9cf228c8e04edc69ad20b1d06ac4e
destroyedlolo <l_faillie@yahoo.com>
2019-04-21 23:48:42 +0200

    Menage sur un exemple inutile

--- ConfigTestTracker/02_TestTracker/DoneTask.lua
+++
@@ -1,9 +0,0 @@
--- This task will be called when the tracker's status changed to Done
---
--- The tracker to follow
--->> whenDone=TestTracker
---
--- Disable this script
---->> disabled
-
-print "The tracker is done"
--- ConfigTestTracker/02_TestTracker/StopTask.lua
+++
@@ -1,8 +0,0 @@
--- This task will be called when the tracker's status changed to Stop
--- The tracker to follow
--->> whenStopped=TestTracker
---
--- Disable this script
---->> disabled
-
-print("The tracker is stopped, while tracker status was", MAJORDOME_TRACKER_STATUS)
