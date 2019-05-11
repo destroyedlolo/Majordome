ffe380eebf0ac3b5e77c745ada575f297e4eb5fc
destroyedlolo <l_faillie@yahoo.com>
2019-05-12 01:54:20 +0200

    Debut de la surveillance de la chambre d'amis en place

---
+++ MaMaison/60_ChAmis/LanceSurveillanceChAmis.lua
@@ -0,0 +1,25 @@
+-- Cette fonction lance la surveillance de température en fonction du mode de la chambre d'amis
+--
+-- Ces 2 timers peuvent déclencher la surveillance
+-->> when=DebutSurveillance
+-->> when=DebutSurveillanceChAmisVacances
+
+--
+-- Récupération des objets cibles
+--
+
+local mode = SelShared.Get("ModeChAmis") or "Manuel"
+local trackersurveillance = MajordomeTracker.find("SurveillanceChAmis", true)
+
+--
+-- C'est parti
+--
+
+if (MAJORDOME_TIMER == "DebutSurveillance" and mode ~= "Vacances") or
+   (MAJORDOME_TIMER == "DebutSurveillanceChAmisVacances" and mode == "Vacances")
+then
+	SelLog.log('D', "Début de la surveillance de la chambre d'amis dû à ".. MAJORDOME_TIMER)
+	trackersurveillance:setStatus("START")
+else
+	SelLog.log('D', "Le début de la surveillance de la chambre d'amis dû à ".. MAJORDOME_TIMER .. " est ignoré")
+end
