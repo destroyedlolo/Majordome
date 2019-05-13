5c91f2b8379d0f8ff9adc23bd32fff870fa347d7
destroyedlolo <l_faillie@yahoo.com>
2019-05-14 01:20:41 +0200

    Suppression d'un message de debug

--- MaMaison/50_VoletChat/ActiveTrappeChat.lua
+++ MaMaison/50_VoletChat/ActiveTrappeChat.lua
@@ -6,7 +6,6 @@ local TmrTrappeChat = MajordomeTimer.find("MyTrappeChat", true)
 if SelShared.Get("ModeForce") ~= "Absent" then
 	if not TmrTrappeChat:isEnabled() then
 		TmrTrappeChat:Enable()
-print("****************")
 		SelLog.log('I', "Activation du timer de la trappe du chat")
 	end
 end
