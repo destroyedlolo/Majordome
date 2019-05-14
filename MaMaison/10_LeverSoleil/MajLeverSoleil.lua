f2438346160b07eed9103b3f0a6eb0033f6b1d93
destroyedlolo <l_faillie@yahoo.com>
2019-05-14 23:31:33 +0200

    Décalage des consignes solaire l'hiver pour faire des économies

--- MaMaison/10_LeverSoleil/MajLeverSoleil.lua
+++ MaMaison/10_LeverSoleil/MajLeverSoleil.lua
@@ -4,6 +4,14 @@ local timer = MajordomeTimer.find("LeverSoleil", true)
 
 local h,m = string.match(string.gsub( SelShared.Get('LeverSoleil'), '%.', ':'), "(%d+):(%d+)")
 
-timer:setAtHM( h, m )
-
 SelLog.log('I', "Le soleil se leve à ".. h ..":".. m)
+
+local saison = SelShared.Get("Saison") or "Hiver"
+if saison == "Hiver" then
+	timer:setAtHM( h, m+30 )
+	h,m = timer:getAtHM()
+	SelLog.log('I', "Comme c'est l'ouvert l'information \"Lever Soleil\" est repoussée à ".. h ..":".. m)
+else
+	timer:setAtHM( h, m )
+end
+
--- MaMaison/10_LeverSoleil/README.md
+++ MaMaison/10_LeverSoleil/README.md
@@ -2,3 +2,5 @@
 ----
 
 L'heure de lever du soleil provient de la météo (par l'intermédiaire de "*Marcel*").
+
+**Note :** Pour faire des économies l'hiver, la consigne "Lever Soleil" est décalée de 30 minutes.
