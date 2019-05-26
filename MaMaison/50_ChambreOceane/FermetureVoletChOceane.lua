5621d178d34175f1cf493624d55f0269c0d71075
destroyedlolo <l_faillie@yahoo.com>
2019-05-26 21:41:17 +0200

    La fermeture des volets fonctionne

--- MaMaison/50_ChambreOceane/FermetureVoletChOceane.lua
+++ MaMaison/50_ChambreOceane/FermetureVoletChOceane.lua
@@ -5,22 +5,16 @@
 -- Récupération des objets cibles
 --
 
-local CmdVoletBureau = MajordomeMQTTTopic.find("CmdVoletBureau", true)
+local CmdVolet = MajordomeMQTTTopic.find("CmdVoletChOceane", true)
 local mode = SelShared.Get("Mode") or "Manuel"
-local saison = SelShared.Get("Saison") or "Intersaison"
-local FermetureVoletBureau = MajordomeTimer.find("FermetureVoletBureau", true)
 
 --
 -- C'est parti
 --
 
 if mode == "Manuel" then
-	SelLog.log('I', "Mode \"Manuel\" : 'Fermeture' du volet du bureau ignoré")
+	SelLog.log('I', "Mode \"Manuel\" : 'Fermeture' du volet de la chambre d'Océane ignoré")
 else
-	if MAJORDOME_TIMER == "CoucherSoleil" and saison == "Hiver" then
-		SelLog.log('D', "'Fermeture' du volet du bureau ignoré (déja fermé car 'hiver')")
-	else
-		SelLog.log('A', "'Fermeture' du volet du bureau")
-		CmdVoletBureau:Publish("Down")
-	end
+	SelLog.log('A', "'Fermeture' du volet de la chambre d'Océane")
+	CmdVolet:Publish("Down")
 end
