a7e0f61a7fec4b0a4b168d9c814c3224516e652b
destroyedlolo <l_faillie@yahoo.com>
2019-06-30 21:43:25 +0200

    Correction des messages

--- MaMaison/60_Cuisine/FinDeJourneeCuisine.lua
+++ MaMaison/60_Cuisine/FinDeJourneeCuisine.lua
@@ -4,9 +4,9 @@
 local mode = SelShared.Get("Mode") or "Manuel"
 
 if mode == "Manuel" then
-	SelLog.log('I', "Mode \"Manuel\" : 'Ouverture' du volet de la cuisine ignoré")
+	SelLog.log('I', "Mode \"Fermeture\" : 'Ouverture' du volet de la cuisine ignoré")
 else
-	SelLog.log('A', "'Ouverture' du volet de la cuisine")
+	SelLog.log('A', "'Fermeture' du volet de la cuisine")
 
 	local CmdVoletCuisine = MajordomeMQTTTopic.find("CmdVoletCuisine", true)
 	CmdVoletCuisine:Publish("Down")
