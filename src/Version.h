ab7c01bf548b92231ccae2fa23fd7e392de6088d
destroyedlolo <l_faillie@yahoo.com>
2020-08-05 02:47:49 +0200

    Add %ClientID% alias to topic

--- src/Version.h
+++ src/Version.h
@@ -1,7 +1,23 @@
 /* Majordome's version
  *
- * 16/03/2019 - Split from Majordome.cpp
+ * Copyright 2018-2020 Laurent Faillie
+ *
+ * 		Majordome is covered by
+ *		Creative Commons Attribution-NonCommercial 3.0 License
+ *      (http://creativecommons.org/licenses/by-nc/3.0/) 
+ *      Consequently, you're free to use if for personal or non-profit usage,
+ *      professional or commercial usage REQUIRES a commercial licence.
+ *  
+ *      Majordome is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *	05/07/2018 - LF - Start of development
+ *	25/07/2018 - LF - Switch to C++ (when it's useful)
+ *	02/10/2018 - LF - Start logging before subscribing in order to get early
+ *		messages as well
+ *	16/03/2019 - LF - Externalize Version
  */
 
-#define VERSION 3.0701
-
+#define VERSION 3.0800
+#define COPYRIGHT "Majordome (c) L.Faillie 2015-2020"
