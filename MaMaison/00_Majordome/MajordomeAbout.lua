2775db86e95fd55ba3b828cdc57e976fdf27154a
destroyedlolo <l_faillie@yahoo.com>
2020-08-06 01:19:53 +0200

    Do some cleaning

--- MaMaison/00_Majordome/MajordomeAbout.lua
+++
@@ -1,7 +0,0 @@
--- Publish about information when anything is received on listened topic
---
--->> listen=MajordomeAbout
---
-
-SelLog.log( 'I', "Majordome's version ".. MAJORDOME_VERSION)
-SelLog.log( 'I', MAJORDOME_COPYRIGHT)
