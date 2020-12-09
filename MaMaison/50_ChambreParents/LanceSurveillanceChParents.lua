ada8a4798d240b243cb205ad0ba34fc7c4a0a4d3
destroyedlolo <l_faillie@yahoo.com>
2020-12-09 21:24:33 +0100

    Oups, il faut sortir

--- MaMaison/50_ChambreParents/LanceSurveillanceChParents.lua
+++ MaMaison/50_ChambreParents/LanceSurveillanceChParents.lua
@@ -3,6 +3,7 @@
 -- Ces 2 timers peuvent déclencher la surveillance
 -->> when=DebutSurveillance
 -->> when=DebutSurveillanceVacances
+-->> when=OuvertureVoletChParents
 
 --
 -- Récupération des objets cibles
@@ -10,10 +11,33 @@
 
 local mode = SelShared.Get("ModeChParents") or "Manuel"
 local trackersurveillance = MajordomeTracker.find("SurveillanceChParents", true)
+local timerouverture = MajordomeTimer.find("OuvertureVoletChParents", true)
 
 --
 -- C'est parti
 --
+
+-- quelle est la consigne de début de surveillance est a utilisée ?
+local tmr_srv
+if mode == "Vacances" then
+	tmr_srv = MajordomeTimer.find("DebutSurveillanceVacances", true)
+	MAJORDOME_TIMER = "DebutSurveillanceVacances"	-- force la raison du lancement
+else
+	tmr_srv = MajordomeTimer.find("DebutSurveillance", true)
+	MAJORDOME_TIMER = "DebutSurveillance"
+end
+SelLog.log('D', "Consigne de début de surveillance ChParent : " .. tmr_srv:getAt());
+
+-- vérifie que la consigne est < à l'heure de levé
+if tmr_srv:getAt() < timerouverture:getAt() then
+	SelLog.log('D', "Le début de la surveillance de la chambre des parents ignoré (".. MAJORDOME_TIMER .. " < consigne )")
+	return
+end
+
+-------
+-- Ajouter le mode DELSTART ou le démarrage démarre après 1 minute (en cas de démarrage par la
+-- consigne de lever
+------
 
 	-- En fonction du mode, vérifie que c'est le bon timer qui tente de
 	-- réveiller ce tracker
