f1e692d2f530404fff089d5bc9d0587bcbe0b333
destroyedlolo <l_faillie@yahoo.com>
2020-12-10 01:50:42 +0100

    La surveillance ne débutera après l'ouverture des volets

--- MaMaison/50_ChambreParents/LanceSurveillanceChParents.lua
+++ MaMaison/50_ChambreParents/LanceSurveillanceChParents.lua
@@ -13,39 +13,34 @@ local mode = SelShared.Get("ModeChParents") or "Manuel"
 local trackersurveillance = MajordomeTracker.find("SurveillanceChParents", true)
 local timerouverture = MajordomeTimer.find("OuvertureVoletChParents", true)
 
+-- Quelle est la consigne active
+local consigne
+if mode == "Vacances" then
+	consigne = MajordomeTimer.find("DebutSurveillanceVacances", true)
+else
+	consigne = MajordomeTimer.find("DebutSurveillance", true)
+end
+
 --
 -- C'est parti
 --
 
--- quelle est la consigne de début de surveillance est a utilisée ?
-local tmr_srv
-if mode == "Vacances" then
-	tmr_srv = MajordomeTimer.find("DebutSurveillanceVacances", true)
-	MAJORDOME_TIMER = "DebutSurveillanceVacances"	-- force la raison du lancement
-else
-	tmr_srv = MajordomeTimer.find("DebutSurveillance", true)
-	MAJORDOME_TIMER = "DebutSurveillance"
-end
-SelLog.log('D', "Consigne de début de surveillance ChParent : " .. tmr_srv:getAt());
+if MAJORDOME_TIMER == consigne:getName() then	-- Lancé par la consigne
+	if consigne:getAt() < timerouverture:getAt() then
+		SelLog.log('D', "Le début de la surveillance de la chambre des parents est reporté (".. MAJORDOME_TIMER .. " < consigne )")
+		return
+	end
 
--- vérifie que la consigne est < à l'heure de levé
-if tmr_srv:getAt() < timerouverture:getAt() then
-	SelLog.log('D', "Le début de la surveillance de la chambre des parents ignoré (".. MAJORDOME_TIMER .. " < consigne )")
-	return
+elseif MAJORDOME_TIMER == "OuvertureVoletChParents" then -- lancé par l'heure de levé
+	if consigne:getAt() > timerouverture:getAt() then	-- ignoré car ce n'est pas encore l'heure de la consigne
+		SelLog.log('D', "Le début de la surveillance de la chambre des parents est reporté à consigne")
+		return
+	end
+
+	SelLog.log('D', "Début de la surveillance de la chambre est retardée de 30 secondes")
+	Selene.Sleep(30);	-- on laisse les volets s'ouvrir
+
 end
 
--------
--- Ajouter le mode DELSTART ou le démarrage démarre après 1 minute (en cas de démarrage par la
--- consigne de lever
-------
-
-	-- En fonction du mode, vérifie que c'est le bon timer qui tente de
-	-- réveiller ce tracker
-if (MAJORDOME_TIMER == "DebutSurveillance" and mode ~= "Vacances") or
-   (MAJORDOME_TIMER == "DebutSurveillanceVacances" and mode == "Vacances")
-then
-	SelLog.log('D', "Début de la surveillance de la chambre des parents dû à ".. MAJORDOME_TIMER)
-	trackersurveillance:setStatus("START")
-else
-	SelLog.log('D', "Le début de la surveillance de la chambre des parents dû à ".. MAJORDOME_TIMER .. " est ignoré")
-end
+SelLog.log('D', "Début de la surveillance de la chambre des parents dû à ".. MAJORDOME_TIMER)
+trackersurveillance:setStatus("START")
