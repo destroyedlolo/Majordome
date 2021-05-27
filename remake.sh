f608b8d3d35cccc1694ae7b8311c526708c12a6d
destroyedlolo <l_faillie@yahoo.com>
2021-05-27 22:02:14 +0200

    Secure Lua finding (tested with Lua 5.1 and 5.4)

--- remake.sh
+++ remake.sh
@@ -2,22 +2,48 @@
 
 #this script is used to create a Makefile
 
+echo
+echo "Prerequisites"
+echo "-------------"
+echo
+
+echo -n "Selene ..."
+
 if [ -d /usr/local/lib/Selene ]; then
 	echo "System installation"
 	SELLIBDIR=/usr/local/lib
-elif [ -d ~/Projets/Selene.v4 ]; then
-	echo "**DEV**DEV** V4"
-	SELLIBDIR=~/Projets/Selene.v4
 else
 	echo "**DEV**DEV**"
 	SELLIBDIR=~/Projets/Selene
 fi
-echo "----------------"
+
+# system Lua
+VERLUA=$( lua -v 2>&1 | grep -o -E '[0-9]\.[0-9]' )
+
+echo -n "Lua's version :" $VERLUA " ... "
+
+if pkg-config --cflags lua$VERLUA > /dev/null 2>&1; then
+	echo "Found Lua$VERLUA package"
+	LUA="\$(shell pkg-config --cflags lua$VERLUA )"
+	LUALIB="\$(shell pkg-config --libs lua$VERLUA )"
+elif pkg-config --cflags lua > /dev/null 2>&1; then
+	echo "Found Lua package"
+	LUA="\$(shell pkg-config --cflags lua )"
+	LUALIB="\$(shell pkg-config --libs lua )"
+else
+	echo "Lua not found"
+	exit 1
+fi
+
+echo
+echo "Source configuration"
+echo "--------------------"
 echo
 
 SELLIBHDIR=$SELLIBDIR/src/SeleneLibrary
 
 cd src
 
-LFMakeMaker -v +f=Makefile -cc="g++ -Wall -O2 -DDEBUG -lpthread -lpaho-mqtt3c `pkg-config --cflags lua` `pkg-config --libs lua` \
+LFMakeMaker -v +f=Makefile -cc="g++ -Wall -O2 -DDEBUG -lpthread -lpaho-mqtt3c \
+$LUA $LUALIB \
 -I$SELLIBHDIR -L$SELLIBDIR -lSelene" *.cpp -t=../Majordome > Makefile
