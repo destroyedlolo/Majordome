066dee4dded8e8953da3270b4699c1d0f34ad0e0
destroyedlolo <l_faillie@yahoo.com>
2024-06-13 00:31:59 +0200

    Correct Saison

--- MaMaison/08_Saison/LogSaison.lua
+++ MaMaison/08_Saison/LogSaison.lua
@@ -1,4 +1,4 @@
---->> listen=Saison
+-->> listen=Saison
 -->> require_topic=Saison
 
-SelLog.log('M', "La saison est '".. Saison:getVal() .."'")
+SelLog.Log('M', "La saison est '".. Saison:getVal() .."'")
