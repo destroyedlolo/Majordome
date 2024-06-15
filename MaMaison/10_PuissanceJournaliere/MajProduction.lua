04416b126fc9f2aa924f51fe44f1eab3aca03015
destroyedlolo <l_faillie@yahoo.com>
2024-06-15 18:16:00 +0200

    Corrige les logs

--- MaMaison/10_PuissanceJournaliere/MajProduction.lua
+++ MaMaison/10_PuissanceJournaliere/MajProduction.lua
@@ -3,7 +3,7 @@
 
 if not SelSharedVar.Get("PRODCNT") then	-- Première valeur du jour
 	SelSharedVar.Set("PRODCNT", SelSharedVar.Get("TeleInfoProdCnt") )
-	SelLog.log('T', "Init du compteur de conso HC : ".. SelSharedVar.Get("TeleInfoProdCnt"))
+	SelLog.Log('T', "Init du compteur de conso HC : ".. SelSharedVar.Get("TeleInfoProdCnt"))
 end
 
 local pwr = 0
@@ -12,7 +12,7 @@ if SelSharedVar.Get("PRODCNT") and SelSharedVar.Get("TeleInfoProdCnt") then
 	pwr = pwr + SelSharedVar.Get("TeleInfoProdCnt") - SelSharedVar.Get("PRODCNT")
 end
 
-SelLog.log('T', "Puissance produite : ".. pwr)
+SelLog.Log('T', "Puissance produite : ".. pwr)
 
 local PubProdJournee = MajordomeMQTTTopic.find("PubProdJournee", true)
 PubProdJournee:Publish(pwr)
