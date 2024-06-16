deca7b688e33e907aa309948c52570336574a6f3
destroyedlolo <l_faillie@yahoo.com>
2024-06-16 17:45:03 +0200

    Ajoute de la chambre d'OcÃane

---
+++ MaMaison/50_ChambreOceane/MyVoletChOceane.lua
@@ -0,0 +1,36 @@
+-- Fermeture "My" du volet de la chambre d'Océane
+--
+-- Ouverture du matin
+-->> when=OuvertureVoletChOceaneMy
+--
+-- Fermeture du soir
+-->> when=ConsigneCoucherMy
+--
+-- Suivit de la température
+-->> whenDone=SurveillanceChOceane
+--
+-->> require_topic=ModeChJoris
+
+--
+-- Récupération des objets cibles
+--
+
+local CmdVolet = MajordomeMQTTTopic.find("CmdVoletChOceane", true)
+
+--
+-- C'est parti
+--
+
+if ModeChOceane:getVal() == "Manuel" then
+	SelLog.log('I', "Mode \"Manuel\" : 'My' du volet de la chambre d'Océane ignoré")
+else	-- Vérifie en fonction de ce qui a lancer l'exécution
+	if MAJORDOME_TIMER == "ConsigneCoucherMy" then
+		if ModeChOceane:getVal() == "Absent" then
+			SelLog.Log('I', "Mode \"Absent\" : 'My' de fin de journée du volet de la chambre d'Océane ignoré")
+			return
+		end
+	end
+
+	SelLog.Log('A', "'My' du volet de la chambre d'Océane")
+	CmdVolet:Publish("My")
+end
