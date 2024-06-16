c21c56a339ff7cec7389272abd75b49f9b424fe0
destroyedlolo <l_faillie@yahoo.com>
2024-06-16 17:09:58 +0200

    Ajt Chambre Joris

---
+++ MaMaison/50_ChambreJoris/calcModeChJoris.lua
@@ -0,0 +1,20 @@
+-- Calcul du mode réel pour la chambre de Joris
+-->> listen=Mode
+-->> require_topic=Mode
+-->> listen=ModeForceEnfants
+-->> need_topic=ModeForceEnfants
+-->> listen=ModeForceJoris
+-->> need_topic=ModeForceJoris
+-->> need_topic=ModeChJoris
+
+local res = SelSharedVar.Get("ModeForceJoris") or "Auto"
+
+if res == "Auto" then
+	res = ModeForceEnfants:getVal()
+	if res == "Auto" then
+		res = Mode:getVal()
+	end
+end
+
+ModeChJoris:Publish(res)
+SelLog.Log('M', "Le mode de la chambre de Joris est : " .. res)
