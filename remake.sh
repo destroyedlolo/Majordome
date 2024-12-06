f2ffb2e4a9b8607120f320debeb07b1363dac13e
destroyedlolo <l_faillie@yahoo.com>
2024-12-06 22:51:16 +0100

    Mode development

--- remake.sh
+++ remake.sh
@@ -37,6 +37,8 @@ else
 fi
 
 echo -n "Selene : "
+
+: <<'DEV'
 if [ -f /usr/local/lib/libSelene.so.2 ]; then
 	echo "System installation"
 	SELDIR=/usr/local
@@ -46,10 +48,13 @@ elif [ -d ~/Projets/Selene.v7 ]; then
 	SELDIR=~/Projets/Selene.v7
 	SELLIB='-l:libSelene.so.2'
 else
+DEV
 	echo "**DEV**DEV**"
 	SELDIR=~/Projets/Selene
 	SELLIB='-l:libSelene.so.2'
-fi
+	echo "Don't forget"
+	echo export LD_LIBRARY_PATH=$SELDIR/lib:$LD_LIBRARY_PATH
+# fi
 
 echo
 echo "----------------"
