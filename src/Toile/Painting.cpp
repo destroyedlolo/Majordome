6c666e5924d1806aab200cce0d5c35d733e0c32a
destroyedlolo <l_faillie@yahoo.com>
2024-12-07 01:50:06 +0100

    Add constructor

--- src/Toile/Painting.cpp
+++ src/Toile/Painting.cpp
@@ -4,6 +4,18 @@
 
 #include "Painting.h"
 
+#include <iostream>
+#include <fstream>
+#include <sstream>	// stringstream
+
+#include <cstring>
+#include <cassert>
+
+extern "C" {
+    #include "lualib.h"
+    #include "lauxlib.h"
+};
+
 Painting::Painting( const std::string &fch, std::string &where, std::string &name, lua_State *L ): surface(NULL){
 	if(verbose)
 		SelLog->Log('L', "\t'%s'", fch.c_str());
@@ -12,4 +24,57 @@ Painting::Painting( const std::string &fch, std::string &where, std::string &nam
 	this->name = name;
 	this->where = where;
 
+	/*
+	 * Reading file's content
+	 */
+
+	std::stringstream buffer;
+	std::ifstream file;
+	file.exceptions ( std::ios::eofbit | std::ios::failbit );
+	try {
+		std::ifstream file(fch);
+		std::streampos pos;
+
+		bool nameused = false;	// if so, the name can't be changed anymore
+
+		do {
+			std::string l;
+			pos = file.tellg();
+
+			std::getline( file, l);
+			if( l.compare(0, 2, "--") ){	// End of comments
+				file.seekg( pos );
+				break;
+			}
+
+			MayBeEmptyString arg;
+			if( !!(arg = striKWcmp( l, "-->> name=" ))){
+				if( nameused ){
+					SelLog->Log('F', "\t\tName can be changed only before listen, until or waitfor directives");
+					exit(EXIT_FAILURE);
+				}
+
+				this->name = name = arg;
+				if(verbose)
+					SelLog->Log('C', "\t\tChanging name to '%s'", name.c_str());
+			} else if( LuaExec::readConfigDirective(l) )
+				nameused = true;
+		} while(true);
+
+		/*
+		 * Reading the remaining of the script and keep it as 
+		 * an Lua's script
+		 */
+
+		buffer << file.rdbuf();
+		file.close();
+	} catch(const std::ifstream::failure &e){
+		if(!file.eof()){
+			SelLog->Log('F', "%s : %s", fch.c_str(), strerror(errno) );
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if( !this->LoadFunc( L, buffer, this->name.c_str() ))
+		exit(EXIT_FAILURE);
 }
