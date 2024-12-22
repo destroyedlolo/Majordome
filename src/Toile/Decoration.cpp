0f49a0ea05017eb4b9d13bec6e8956f8fe5dec92
destroyedlolo <l_faillie@yahoo.com>
2024-12-22 19:33:15 +0100

    Introduce Decoration

---
+++ src/Toile/Decoration.cpp
@@ -0,0 +1,12 @@
+/* Decoration object
+ * 	Task to be called when an object needs to be refreshed
+ */
+
+#include "Decoration.h"
+
+extern "C" {
+    #include "lualib.h"
+    #include "lauxlib.h"
+};
+
+
---
+++ src/Toile/Decoration.h
@@ -0,0 +1,20 @@
+/* Decoration object
+ * 	Task to be called when an object needs to be refreshed
+ *
+ * 	19/12/2024 - First version
+ */
+
+#ifndef DECORATION_H
+#define DECORATION_H
+
+#include "../Config.h"
+#include "../Helpers.h"
+#include "../Object.h"
+#include "../LuaExec.h"
+
+#include <Selene/SelGenericSurface.h>
+
+class Decoration : public LuaExec {
+};
+
+#endif
