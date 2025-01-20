3e016ccd5615b9da79a7928993c7c0f0c700f936
destroyedlolo <l_faillie@yahoo.com>
2025-01-20 18:05:06 +0100

    Add Object

---
+++ src/Object.cpp
@@ -0,0 +1,49 @@
+#include "Object.h"
+#include "Selene.h"
+#include "Helpers.h"
+
+Object::Object(const std::string &fch, std::string &where, std::string &name) : disabled(false), quiet(false){
+	if(verbose)
+		SelLog->Log('L', "\t'%s'", fch.c_str());
+
+	this->extrName( fch, name );
+	this->name = name;
+	this->where = where;
+}
+
+void Object::extrName( const std::string &fch, std::string &name){
+	name = fch;
+	const size_t last_slash_idx = name.find_last_of("/");	// Filename only
+	if(std::string::npos != last_slash_idx)
+		name.erase(0, last_slash_idx + 1);
+
+	const size_t period_idx = name.rfind('.');	// Remove extension if present.
+	if (std::string::npos != period_idx)
+		name.erase(period_idx);
+}
+
+void Object::readConfigDirective(std::string &l, bool &nameused){
+	MayBeEmptyString arg;
+
+	if( !!(arg = striKWcmp( l, "-->> name=" ))){
+		if( nameused ){
+			SelLog->Log('F', "\t\tName can be changed only before any other directives");
+			exit(EXIT_FAILURE);
+		}
+
+		this->name = name = arg;
+		if(verbose)
+			SelLog->Log('C', "\t\tChanging name to '%s'", name.c_str());
+	} else if( l == "-->> quiet" ){
+		if(verbose)
+			SelLog->Log('C', "\t\tBe quiet");
+		this->beQuiet();
+	} else if( l == "-->> disabled" ){
+		if(verbose)
+			SelLog->Log('C', "\t\tDisabled");
+		this->disable();
+	} else if(!! striKWcmp( l, "-->> ")){
+		SelLog->Log('F', "Unknown directive '%s'", l.c_str());
+		exit(EXIT_FAILURE);
+	}
+}
---
+++ src/Object.h
@@ -0,0 +1,58 @@
+/* Generic class for objects
+ *
+ * 27/07/2018 - LF - First version
+ */
+
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <string>
+
+class Object {
+	bool disabled;
+	bool quiet;
+
+protected:
+	std::string name;
+	std::string where;
+
+		/* Read configuration directive
+		 * l -> string to read
+		 * nameused -> boolean holding 'true' if the object's name has been
+		 *		already stored in a list. So the name can't be change anymore.
+		 *
+		 * NOTEZ-BIEN : this function will FAIL if the directive is not 
+		 * 		recognized ; it has to be called lastly.
+		 */
+	virtual void readConfigDirective(std::string &l, bool &nameused);
+
+public:
+	Object() : disabled(false), quiet(false){};
+	Object(const std::string &fch, std::string &where, std::string &name);
+
+	Object& operator=(const Object&) = default;	/* avoid "defaulted move" warning */
+
+	void enable( void ){ this->disabled = false; };
+	void disable( void ){ this->disabled = true; };
+	bool isEnabled( void ){ return !this->disabled; };
+
+		// Remove some messages
+	void beQuiet( void ){ this->quiet = true; };
+	void beNoisy( void ){ this->quiet = false; };
+	bool isQuiet( void ){ return this->quiet; };
+
+	std::string &getName( void ){ return this->name; };	// Object's name
+	const char *getNameC( void ){ return this->name.c_str(); };
+	std::string &getWhere( void ){ return this->where; }; // Object's container (it's father directory)
+	const char *getWhereC( void ){ return this->where.c_str(); };
+
+	/*
+	 * determine the name from the filename
+	 * -> fch : filename (optionally including its path)
+	 * <- name : extracted name
+	 */
+	void extrName( const std::string &fch, std::string &name );
+
+};
+
+#endif
