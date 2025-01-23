7d3c8acda7e70dac2657c3a8844e412885b64b4d
destroyedlolo <l_faillie@yahoo.com>
2025-01-23 19:31:14 +0100

    Update example with new syntax

--- Documentations/SamplesCode/Rendez-Vous/01_Sample_Rendez-Vous/Startup.lua
+++ Documentations/SamplesCode/Rendez-Vous/01_Sample_Rendez-Vous/Startup.lua
@@ -1,13 +1,9 @@
 -- Script launched after loading
 --
 -->> RunAtStartup
-
--- Find out rendez-vous
-local rdv =  MajordomeRendezVous.find("example")
-
--- check if it's found
-assert(rdv, "Can't find rendez-vous")
-SelLog.Log("Rendez-vous found, lets launch it")
+--
+-- Create the rendez-vous object
+-->> need_rendezvous=example
 
 -- Launch it
-rdv:Launch()
+example:Launch()
