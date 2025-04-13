05bf0a789589a77c432ff9c42af01349978de3b6
destroyedlolo <l_faillie@yahoo.com>
2025-04-14 00:21:07 +0200

    Cleaning

--- Test/10_Archive/notififyDone.lua
+++
@@ -1,5 +0,0 @@
--- Log when the archiving is over
---
--->> waitfor=ArchivingDone
-
-SelLog.Log('I', "Temperatures archiving is done")
--- Test/10_Archive/notififyFailed.lua
+++
@@ -1,5 +0,0 @@
--- Log when the archiving is over
---
--->> waitfor=ArchivingFailed
-
-SelLog.Log('I', "Temperatures archiving has failed")
