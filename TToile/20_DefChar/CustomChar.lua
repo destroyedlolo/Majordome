d3c78ac079acd185915c1f68b486c084d915800e
destroyedlolo <l_faillie@yahoo.com>
2025-08-16 02:10:27 +0200

    20x4

---
+++ TToile/20_DefChar/CustomChar.lua
@@ -0,0 +1,55 @@
+-->> desc=Define custom chars
+-->> RunAtStartup
+-->> need_renderer=LCD
+
+-- Define custom characters
+-- char 0-2 are already used by the clock
+
+-- 3 : Photovoltaic
+LCD:SetChar(3, {
+	"     ",
+	"  XXx",
+	" xxx ",
+	"xxx  ",
+	"  xx ",
+	" xx  ",
+	"x    ",
+	"     "
+})
+
+-- 4 : Consumption
+LCD:SetChar(4, {
+	"     ",
+	" x x ",
+	"xxxxx",
+	"xxxxx",
+	" xxx ",
+	"  x  ",
+	"  x  ",
+	"  x  "
+})
+
+-- 5 : powersaving
+LCD:SetChar(5, {
+	" xXX ",
+	"x   X",
+	"XXX  ",
+	"X    ",
+	"XXX  ",
+	"x   X",
+	" xXx ",
+	"     "
+})
+
+-- 6 : Volt-Ampere
+LCD:SetChar(6, {
+	"x   x",
+	"x  x ",
+	"x x  ",
+	"xx  x",
+	"x  xx",
+	"  x x",
+	" xxxx",
+	"x   x"
+})
+
