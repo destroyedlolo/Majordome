cea101e3643f49bc716a4629506b0021b770bbca
destroyedlolo <l_faillie@yahoo.com>
2025-08-29 21:12:24 +0200

    Start with Resources

---
+++ src/Resource.h
@@ -0,0 +1,37 @@
+/* Resource.h
+ *	Reduce or avoid concurrent access.
+ *
+ *	29/08/2025 - LF - First version
+ */
+
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#include <mutex>
+#include <condition_variable>
+
+#include <cstdint>
+
+class Semaphore {
+	std::mutex mutex;				// Avoid concurrent connection
+	std::condition_variable cond;	// Condition
+	uint8_t counter;				// Counter of running tasks
+
+public:
+	explicit Semaphore(uint8_t max) : counter(max) {}
+
+	void acquire() {
+		std::unique_lock<std::mutex> lock(this->mutex);
+		this->cond.wait(lock, [this]() { return this->counter > 0; });
+        --this->counter;
+    }
+
+	void release() {
+		std::lock_guard<std::mutex> lock(this->mutex);
+		++this->counter;
+		this->cond.notify_one();
+    }
+
+};
+
+#endif
