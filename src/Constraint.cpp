4e51f885228c164c60dda3d39925b39acbb5a595
destroyedlolo <l_faillie@yahoo.com>
2025-09-02 00:54:18 +0200

    Wip

---
+++ src/Constraint.cpp
@@ -0,0 +1,64 @@
+#include "Config.h"
+#include "Constraint.h"
+#include "Helpers.h"
+
+bool Constraint::readConfigDirective(std::string &l){
+	std::string arg;
+
+	if(!(arg = striKWcmp( l, "-->> resourceWait=" )).empty()){
+		ResourceCollection::iterator resource;
+
+		if( (resource = config.ResourcesList.find(arg)) != config.ResourcesList.end()){
+			this->res = resource->second;
+			this->haveToWait = true;
+
+			if(::verbose)
+				SelLog->Log('C', "\t\tWill wait for resource '%s'", arg.c_str());
+
+			if(d2)
+				fd2 << this->getFullId() << " -- " << resource->second->getFullId() << ": need { class: lwaitresource }" << std::endl;
+			return true;
+		} else {
+			SelLog->Log('F', "\t\tResource '%s' is not (yet ?) defined", arg.c_str());
+			exit(EXIT_FAILURE);
+		}
+	} else 	if(!(arg = striKWcmp( l, "-->> resourceFail=" )).empty()){
+		ResourceCollection::iterator resource;
+
+		if( (resource = config.ResourcesList.find(arg)) != config.ResourcesList.end()){
+			this->res = resource->second;
+			this->haveToWait = true;
+
+			if(::verbose)
+				SelLog->Log('C', "\t\tWill try to acquire resource '%s'", arg.c_str());
+
+			if(d2)
+				fd2 << this->getFullId() << " -- " << resource->second->getFullId() << ": need { class: ltryresource }" << std::endl;
+			return true;
+		} else {
+			SelLog->Log('F', "\t\tResource '%s' is not (yet ?) defined", arg.c_str());
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return false;
+}
+
+bool Constraint::tryToAcquireResource(void){
+	if(res)
+		return res->acquire(false);
+	else
+		return true;	// We can run if no resource attached
+}
+
+bool Constraint::waitForResource(void){
+	if(res)
+		return res->acquire();
+	else
+		return true;
+}
+
+void Constraint::release(void){
+	if(res)
+		res->release();
+}
--- src/Constraint.h
+++ src/Constraint.h
@@ -7,4 +7,21 @@
 #ifndef CONSTRAINT_H
 #define CONSTRAINT_H
 
+#include "Resource.h"
+
+class Constraint {
+	Resource *res;
+	bool haveToWait;
+
+protected:
+	Constraint() : res(NULL) {};
+
+	virtual bool readConfigDirective(std::string &l);
+
+public:
+	bool tryToAcquireResource(void);	// False if the resource if no resources available
+	 bool waitForResource(void);	// Wait until the resource is available
+	void release(void);
+};
+
 #endif
