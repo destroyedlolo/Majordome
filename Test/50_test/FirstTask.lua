d168aaf5352a269753bc788cc000604986d1c8a5
destroyedlolo <l_faillie@yahoo.com>
2025-09-14 01:08:35 +0200

    Test purging vs resources

---
+++ Test/50_test/FirstTask.lua
@@ -0,0 +1,7 @@
+-->> desc=Task having long tun
+-->> resourceWait=once
+-->> RunAtStartup
+
+SelLog.Log("1", "1st task starting")
+os.execute("sleep 3")
+SelLog.Log("1", "1st task finished")
---
+++ Test/50_test/SecondTask.lua
@@ -0,0 +1,7 @@
+-->> desc=Task having long tun
+-->> resourceWait=once
+-->> RunAtStartup
+
+SelLog.Log("2", "2nd task starting")
+os.execute("sleep 3")
+SelLog.Log("2", "2nd task finished")
